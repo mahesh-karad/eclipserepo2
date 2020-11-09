40c3664d4c3473d046053fae1c977fa4242bda5b
mahesh.karad <mahesh.karad@apisero.com>
2020-11-09 10:57:24 +0530

    my first eclipse commit

---
+++ src/com/pk1/OverloadDemo.java
@@ -0,0 +1,24 @@
+package com.pk1;
+
+public class OverloadDemo {
+    public void add(int a,int b)
+    {
+    	System.out.println(a+b);
+    }
+    public void add(float a,int b)
+    {
+    	System.out.println(a+b);
+    }
+    public void add(char a,char b)
+    {
+    	System.out.println(a+b);
+    }
+    
+	public static void main(String[] args) {
+	OverloadDemo o= new OverloadDemo();
+	o.add(10,20);
+	o.add(12.12f, 20);
+	o.add('a', 'b');
+	}
+
+}
